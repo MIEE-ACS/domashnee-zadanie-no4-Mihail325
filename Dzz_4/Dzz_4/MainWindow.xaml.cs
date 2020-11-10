90ba28861b470e80fa412ba6d63619a6101a5ebf
Mihail325 <mihail.molch4noff@yandex.ru>
2020-11-11 00:23:13 +0300

    Выполнил
    не проработал только движение элементов окна при его расширении, но по-моему в нашем случае это не так и важно)

---
+++ Dzz_4/Dzz_4/MainWindow.xaml.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Dzz_4
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        double[] array;
+        uint N = 0u;
+        private void bCreat_Click(object sender, RoutedEventArgs e)
+        {
+            if (uint.TryParse(tbcount.Text, out N) && N > 0)
+            {
+                if (N < 10000)
+                {
+                    array = new double[N];
+                    Random random = new Random();
+                    for (int i = 0; i < N; i++)
+                    {
+                        array[i] = Math.Round(-10 + random.NextDouble() * 20, 3);
+                    }
+                    var Array = array.Aggregate("", (res, x) => res += $" {x:0.000};").ToString();
+                    tboutputM.Text = Array.Remove(Array.Length - 1);
+                }
+                else 
+                {
+                    MessageBox.Show("Не пугай деда!\nУ компьютера тоже может быть инсульта памяти.\nВозьми число поменьше.",
+                        "Ошибка!",
+                        MessageBoxButton.OK,
+                        MessageBoxImage.Error);
+                    tbcount.Clear();
+                    tboutputM.Clear();
+                }
+            }
+            else 
+            {
+                    MessageBox.Show("В данное поле можно вводить только натуральные числа!",
+                        "Ошибка!",
+                        MessageBoxButton.OK,
+                        MessageBoxImage.Error);
+                    tbcount.Clear();
+                tboutputM.Clear();
+            }
+            
+
+        }
+
+        private void bsearchmin_Click(object sender, RoutedEventArgs e)
+        {
+            double min = array[0];
+            for (int i = 1; i < N; i++) 
+            { 
+            if (min>array[i]) 
+                    min = array[i];
+            }
+            tbMin.Text = String.Format($"{min:0.000}");
+        }
+
+        private void bSum_Click(object sender, RoutedEventArgs e)
+        {
+            int indexlastof=0, indexof=0;
+            bool f1 = false, f2 = false;
+            double sum=0;
+            for (int i = 0; i < N; i++)
+            {                
+               
+                if (array[i] > 0 && f1==false)
+                {
+                    indexof = i;
+                    f1 = true;
+                    continue;
+                }
+                if (array[i] > 0)
+                {
+                    indexlastof = i;
+                    f2 = true;
+                }
+            }
+            if (f1 == false)
+                tbsum.Text = "В массиве нет положительных чисел";
+            else if (f2 == false || N==1)
+                tbsum.Text = "В массиве только одно положительно число";
+            else if ((indexlastof - indexof) <= 1)
+                tbsum.Text = "Первый и последний положительные элементы стоят рядом.";
+            else
+            {
+                for (int i = indexof + 1; i < indexlastof; i++)
+                    sum += array[i];
+                tbsum.Text = String.Format($"{sum:0.000}");
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+           //array[0] = 0; array[3] = 0; array[4] = 0; array[6] = 0; array[7] = 0; //-для проверки на нули;
+            bool f1 = false;
+            for (int i = 0; i < array.Length; i++) 
+            {
+                if (array[i] == 0)
+                {
+                    f1 = true;
+                    break;
+                }
+            }
+            if (f1 == false)
+            {
+                tbChangeMas.Text = array.Aggregate("", (res, x) => res += $" {x:0.000};");
+                MessageBox.Show("В данном массиве нет нулей.\nМассив выведен без изменений. ",
+                       "Замечание!",
+                       MessageBoxButton.OK,
+                       MessageBoxImage.Information);
+            }
+            else
+            {
+                var change = array.Where(x => x == 0).Concat(array.Where(x => x != 0)).ToArray();
+                tbChangeMas.Text = change.Aggregate("", (res, x) => res += $" {x:0.000};");
+            }
+        }
+    }
+}
+
+
+    
---
+++ Dzz_4/Dzz_4_part_2/MainWindow.xaml.cs
@@ -0,0 +1,322 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Dzz_4_part_2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        struct saddle
+        {
+            public int value;
+            public int x;
+            public int y;
+        }
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        uint lines = 0, columns = 0;
+        int[,] matrix;
+        int Z;
+        bool f1 = false, f2 = false,f3 = false;
+        int t = 0, k = 0;
+        string str2 = "";
+        private void tbCreat_Click(object sender, RoutedEventArgs e)
+        {
+            int Max = 30;
+            spEveryput.IsEnabled = false;
+            try
+            {
+                lines = uint.Parse(tbCountLines.Text);
+                Equalzero(lines);
+                Over(lines, Max);
+                f1 = true;
+            }
+            catch (OverflowException)
+            {
+                MessageBox.Show("В данное поле можно вводить только натуральные числа!",
+                    "Ошибка!",
+                    MessageBoxButton.OK,
+                    MessageBoxImage.Error);
+                tbCountLines.Clear();
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("В данное поле можно вводить только натуральные числа!",
+                    "Ошибка!",
+                    MessageBoxButton.OK,
+                    MessageBoxImage.Error);
+                tbCountLines.Clear();
+            }
+            catch (EqualNull)
+            {
+                MessageBox.Show("Количество строк должно быть больше 0.",
+                    "Ошибка!",
+                    MessageBoxButton.OK,
+                    MessageBoxImage.Error);
+                tbCountLines.Clear();
+            }
+            catch (OverlinesColumsn)
+            {
+                MessageBox.Show("Слишком большая матрица получится.\nУменьши количество строк.",
+                    "Ошибка!",
+                    MessageBoxButton.OK,
+                    MessageBoxImage.Error);
+                tbCountLines.Clear();
+            }
+            try
+            {
+                columns = uint.Parse(tbCountColumns.Text);
+                Equalzero(columns);
+                Over(columns, Max);
+                f2 = true;
+            }
+            catch (OverflowException)
+            {
+                MessageBox.Show("В данное поле можно вводить только натуральные числа!",
+                    "Ошибка!",
+                    MessageBoxButton.OK,
+                    MessageBoxImage.Error);
+                tbCountColumns.Clear();
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("В данное поле можно вводить только натуральные числа!",
+                    "Ошибка!",
+                    MessageBoxButton.OK,
+                    MessageBoxImage.Error);
+                tbCountColumns.Clear();
+            }
+            catch (EqualNull)
+            {
+                MessageBox.Show("Количество столбцов должно быть больше 0.",
+                    "Ошибка!",
+                    MessageBoxButton.OK,
+                    MessageBoxImage.Error);
+                tbCountColumns.Clear();
+            }
+            catch (OverlinesColumsn)
+            {
+                MessageBox.Show("Слишком большая матрица получится.\nУменьши количество столбцов.",
+                    "Ошибка!",
+                    MessageBoxButton.OK,
+                    MessageBoxImage.Error);
+                tbCountColumns.Clear();
+            }
+            Random rand = new Random();
+            if (f1 == true && f2 == true)
+            {
+                tbOutputPoint.Clear();
+                tbOutputSum.Clear();
+                matrix = new int[lines, columns];
+                if (rbAuto.IsChecked == true)
+                {
+
+                    string str1 = "";
+                    for (int i = 0; i < lines; i++)
+                    {
+                        for (int j = 0; j < columns; j++)
+                        {
+                            matrix[i, j] = rand.Next(-100, 100);
+                            str1 += ($"{matrix[i, j],5}").PadLeft(5) + "\t";
+                        }
+                        str1 += "\n";
+                    }
+                    tbMatrix.Text = str1.ToString();
+                }
+                else if (rbHand.IsChecked == true)
+                {
+                    spEveryput.IsEnabled = true;
+                    tbMatrix.Clear();
+                     f3 = false;
+                     t = 0; k = 0;
+                     str2 = "";
+                    tbEverWelc.Text = $"Введите A(1,1) элемент:";
+                }
+                else
+                {
+                    MessageBox.Show("Необходимо выбрать как заполнять матрицу.",
+                    "Внимание!",
+                     MessageBoxButton.OK,
+                     MessageBoxImage.Information);
+                }
+            }
+        }
+        private void bSum_Click(object sender, RoutedEventArgs e)
+        {
+            int sum = 0;
+            string str = "";
+            for (int i = 0; i < lines; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    if (matrix[i, j] < 0)
+                    {
+                        for (int k = 0; k < columns; k++)
+                        {
+                            sum += matrix[i, k];
+                        }
+                        str += $"Сумма в {i + 1}-ой строке =" + sum + "\n";
+                        sum = 0;
+                        break;
+                    }
+                }
+            }
+            if (str == "")
+                tbOutputSum.Text = "В матрице нет отрицательных элементов";
+            else
+                tbOutputSum.Text = str;
+        }
+
+        private void bput_Click(object sender, RoutedEventArgs e)
+        {
+            f3 = true;
+            while (f3 == true & t < lines)
+            {
+                while (f3 == true & k < columns)
+                {
+                    try
+                    {
+                        Z = int.Parse(tbEveryputelem.Text);
+                        Over(Z, 10000);
+                        tbEveryputelem.Clear();
+                        matrix[t, k] = Z;
+                        str2 += ($"{matrix[t, k],5}").PadLeft(5) + "\t";
+                        f3 = false;
+                    }
+                    catch (FormatException)
+                    {
+                        MessageBox.Show("В данное поле можно вводить только целые числа!",
+                            "Ошибка!",
+                            MessageBoxButton.OK,
+                            MessageBoxImage.Error);
+                        tbEveryputelem.Clear();
+                    }
+                    catch (Overvalue)
+                    {
+                        MessageBox.Show("Слишком большое число давай поменьше",
+                            "Ошибка!",
+                            MessageBoxButton.OK,
+                            MessageBoxImage.Error);
+                        tbEveryputelem.Clear();
+                    }
+                    if (f3 == false)
+                    {
+                        k++;
+                        if (k!=columns)
+                        tbEverWelc.Text = $"Введите A({t + 1},{k+1}) элемент:";
+                    }
+                    f3 = false;
+                }
+                if (k == columns)
+                {
+                    t++;
+                    k = 0;
+                    str2 += "\n";
+                    if (t!=lines)
+                    tbEverWelc.Text = $"Введите A({t + 1},{k + 1}) элемент:";
+                }
+            }
+            if (t == lines)
+            {
+                tbMatrix.Text = str2.ToString();
+                spEveryput.IsEnabled = false;
+                MessageBox.Show("Матрица проинициализирована!",
+                    "Поздравляем!",
+                    MessageBoxButton.OK,
+                    MessageBoxImage.Information);
+            }
+        }
+
+        //функцию рассчитывал из того что если в строке(столбце) два одинаковых элемента,
+        //то это уже не минимум(не максимум) соответсвенно 
+        private void bPoint_Click(object sender, RoutedEventArgs e)
+        {
+           saddle sed=new saddle();
+            bool f4 = true, f5=true;
+            string conclus = "";
+            for (int i = 0; i < lines; i++)
+            {
+                f4 = true;f5 = true;
+                sed.value = matrix[i, 0];
+                sed.y = 0;
+                sed.x = i;
+                for (int j = 0; j < columns; j++)
+                {
+                    if (sed.value>matrix[i,j]) 
+                    {
+                        sed.value = matrix[i, j];
+                        sed.y = j;
+                        sed.x = i;
+                    }
+                }
+                for (int z = 0; z < columns; z++) 
+                {
+                    if (sed.value == matrix[sed.x, z] && z!=sed.y)
+                    {
+                        f5 = false;f4 = false;
+                        break;
+                    }
+                }
+                if (f5 == true)
+                {
+                    for (int r = 0; r < lines; r++)
+                    {
+                        if (sed.value <= matrix[r, sed.y] && r!=sed.x)
+                        {
+                            f4 = false;
+                            break;
+                        }
+                    }
+                }
+                if (f4==true && f5==true)
+                conclus += $"Номер строки:{sed.x + 1}; Номер столбца:{sed.y + 1};\n";
+            }
+            if (conclus == "")
+                tbOutputPoint.Text = "В данной матрице нет седловых точек";
+            else
+            tbOutputPoint.Text = conclus;
+        }
+
+        public class EqualNull : SystemException
+        {
+        }
+        public class OverlinesColumsn : SystemException
+        {
+        }
+        public class Overvalue: SystemException
+        {
+        }
+        static void Equalzero(uint n)
+        {
+            if (n == 0)
+                throw new EqualNull();
+        }
+        static void Over(uint n,int max)
+        {
+            if (n > max)
+                throw new OverlinesColumsn();
+        }
+        static void Over(int n, int max)
+        {
+            if (n > max)
+                throw new Overvalue();
+        }
+
+    }
+}
